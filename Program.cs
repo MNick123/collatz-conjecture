a9df5539136549bcd0d541f9bd0a823a089e132d
Nick <nickiscool2003@gmail.com>
2021-08-19 18:06:33 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+namespace collatz_conjecture
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           first:
+            Console.WriteLine("Collatz conjeture, type any positive number");
+            string numberinputraw = Console.ReadLine();
+
+            try
+            {
+                Convert.ToInt32(numberinputraw);
+            }
+            catch
+            {
+
+                Console.WriteLine("You enter a invid number");
+                goto first;
+            }
+
+
+
+
+
+
+            #region
+            long Number = Convert.ToInt64(numberinputraw);
+
+            long staringnumber = Number;
+
+            Start:
+            if(Number%2 == 0)
+            {
+                Console.WriteLine("number is even");
+                Number = Number / 2;
+                Console.WriteLine(Number);
+                Thread.Sleep(500);
+            }
+            else if(Number == 1)
+            {
+                Console.WriteLine("Your Number is one which starts a loop");
+                Thread.Sleep(-1);
+            }
+            else
+            {
+                Console.WriteLine("Number is odd");
+                Number = Number * 3 + 1;
+                Console.WriteLine(Number);
+                Thread.Sleep(500);
+            }
+            goto Start;
+            #endregion
+
+        }
+
+
+    }
+}
